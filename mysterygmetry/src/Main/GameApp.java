cac6046ef2a9e11bc45ceb65b0cc499294b2ec10
Corth162 <115749225+Corth162@users.noreply.github.com>
2024-11-16 13:26:52 -0600

    Update GameApp.java

--- mysterygmetry/src/Main/GameApp.java
+++ mysterygmetry/src/Main/GameApp.java
@@ -1,19 +1,10 @@
 package Main;
 import javax.swing.*;
-import java.awt.*;
 
 public class GameApp {
     public static void main(String[] args) {
-        JFrame mainFrame = new JFrame("Main Game UI");
-        mainFrame.setSize(500, 500);
-        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Button to open the instructions dialog
-        JButton instructionsButton = new JButton("Instructions");
-        instructionsButton.addActionListener(e -> showInstructions(mainFrame));
 
-        mainFrame.add(instructionsButton);
-        mainFrame.setVisible(true);
     }
 
     private static void showInstructions(JFrame parent) {
@@ -24,3 +15,4 @@ public class GameApp {
 
     }
 }
+
