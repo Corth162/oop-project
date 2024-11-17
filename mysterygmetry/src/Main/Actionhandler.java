b0705a80c16d77b3d78a5d29aa779be38727b321
audmatthews <157422447+audmatthews@users.noreply.github.com>
2024-11-16 23:27:44 -0600

    Update Actionhandler.java

--- mysterygmetry/src/Main/Actionhandler.java
+++ mysterygmetry/src/Main/Actionhandler.java
@@ -14,16 +14,25 @@ public class Actionhandler implements ActionListener {
         System.out.println("Action Command: " + yourchoice);
         gm.playClickSound();  // This will play the click sound
 
+
         switch(yourchoice) {
 
             //objs
-            case "this_lipstick": gm.ev1.this_lipstick(); break;
-            case "closer_lipstick": gm.ev1.closer_lipstick(); break;
-            case "pick_up_lipstick": gm.ev1.pick_up_lipstick(); break;
+            case "this_cup": gm.ev1.this_cup(); break;
+            case "closer_cup": gm.ev1.closer_cup(); break;
+            case "pick_up_cup": gm.ev1.pick_up_cup(); break;
 
             case "this_plant": gm.ev1.this_plant(); break;
             case "closer_plant": gm.ev1.closer_plant(); break;
             case "pick_up_plant": gm.ev1.pick_up_plant(); break;
+
+            case "this_soap":gm.ev1.this_soap(); break;
+            case "closer_soap": gm.ev1.closer_soap(); break;
+            case "pick_up_soap": gm.ev1.pick_up_soap(); break;
+
+            case "this_portrait":gm.ev1.this_portrait(); break;
+            case "closer_portrait": gm.ev1.closer_portrait(); break;
+            case "pick_up_portrait": gm.ev1.pick_up_portrait(); break;
 
             case "this_book": gm.ev1.this_book(); break;
             case "closer_book": gm.ev1.closer_book(); break;
@@ -33,7 +42,15 @@ public class Actionhandler implements ActionListener {
             case "closer_note": gm.ev1.closer_note(); break;
             case "pick_up_note": gm.ev1.pick_up_note(); break;
 
-        //rooms
+            case "this_shirt": gm.ev1.this_shirt(); break;
+            case "closer_shirt": gm.ev1.closer_shirt(); break;
+            case "pick_up_shirt": gm.ev1.pick_up_shirt(); break;
+
+            case "this_bottle": gm.ev1.this_bottle(); break;
+            case "closer_bottle": gm.ev1.closer_bottle(); break;
+            case "pick_up_bottle": gm.ev1.pick_up_bottle(); break;
+
+            //rooms
             //1
             case "left8": gm.rchange.callroom8();break;
             case "right2": gm.rchange.callroom2();break;
@@ -63,6 +80,3 @@ public class Actionhandler implements ActionListener {
         }
     }
 }
-
-
-
