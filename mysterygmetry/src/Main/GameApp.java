c8057702e8de102b1646b5775a45d02f3a6f91c4
Corth162 <115749225+Corth162@users.noreply.github.com>
2024-12-08 16:30:51 -0600

    Update GameApp.java
    comments

--- mysterygmetry/src/Main/GameApp.java
+++ mysterygmetry/src/Main/GameApp.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 
 public class GameApp {
     public static void main(String[] args) {
-
+//was originally going to hold text but i found it easier to just put in main (gm)
 
     }
 
